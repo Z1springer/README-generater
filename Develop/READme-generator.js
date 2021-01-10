b2ef07f75ae1322e47f6ee577be855f87169cd6b
Zach <zdspringer99@gmail.com>
2021-01-10 15:08:58 -0800

    inquirer prompts added and basic template made

---
+++ Develop/READme-generator.js
@@ -0,0 +1,22 @@
+function generateREADME({ primus, secundus, tertius, quartus, quintus, sextus }) {
+    return
+    `#${primus} 
+    
+
+    ## User Story
+    ${sextus}
+
+
+    ## Description
+    ${secundus}
+
+    ## Links to Repo and Deployed Site
+    Link to repo: ${tertius}
+    Link to deployed app: ${quartus}
+
+    ### Technologies Used
+    ${quintus}
+    `
+}
+
+export default generateREADME()
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,64 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+import generateREADME from './READme-generator'
+// Create an array of questions for user input
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            default: 'Project Name',
+            name: 'primus'
+        },
+        {
+            type: 'input',
+            message: 'Describe your project',
+            default: 'Project Description',
+            name: 'secundus'
+        },
+        {
+            type: 'input',
+            message: 'Please Link your project repository',
+            default: 'Repository Link',
+            name: 'tertius'
+        },
+        {
+            type: 'input',
+            message: 'Please Link your deployed application',
+            default: 'Deployed App Link',
+            name: 'quartus'
+        },
+        {
+            type: 'checkbox',
+            message: 'Which technologies were used?',
+            name: 'quintus',
+            choices: [
+                'HTML', 'CSS', 'JavaScript', 'jQuery', 'Node', 'Python',
+            ],
+        },
+        {
+            type: 'input',
+            message: 'Please Enter a User Story',
+            default: 'Here be User Stories',
+            name: 'sextus'
+        }
+    ])
 
-// TODO: Create an array of questions for user input
-const questions = [];
+    // TODO: Create a function to write README file
+    .then((response) => {
+        console.log(response)
+        const readmestring = generateREADME(response)
+        console.log(readmestring)
+        fs.writeToFile("README.md", readmestring, err => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('IT WORK!')
+            }
+        })
+    })
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() { }
