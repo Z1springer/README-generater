f8230119c797e1713a2136877ccc8c0b345c9af4
Zach <zdspringer99@gmail.com>
2021-01-14 09:25:02 -0800

    able to generate bare, bare-bones readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,67 +1,58 @@
 // Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generator = require('./READme-generator')
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utilities/generateMarkdown");
 // Create an array of questions for user input
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            default: 'Project Name',
-            name: 'primus'
-        },
-        {
-            type: 'input',
-            message: 'Describe your project',
-            default: 'Project Description',
-            name: 'secundus'
-        },
-        {
-            type: 'input',
-            message: 'Please Link your project repository',
-            default: 'Repository Link',
-            name: 'tertius'
-        },
-        {
-            type: 'input',
-            message: 'Please Link your deployed application',
-            default: 'Deployed App Link',
-            name: 'quartus'
-        },
-        {
-            type: 'checkbox',
-            message: 'Which technologies were used?',
-            name: 'quintus',
-            choices: [
-                'HTML', 'CSS', 'JavaScript', 'jQuery', 'Node', 'Python',
-            ],
-        },
-        {
-            type: 'input',
-            message: 'Please Enter a User Story',
-            default: 'Here be User Stories',
-            name: 'sextus'
-        }
-    ])
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    default: "Project Name",
+    name: "primus",
+  },
+  {
+    type: "input",
+    message: "Describe your project",
+    default: "Project Description",
+    name: "secundus",
+  },
+  {
+    type: "input",
+    message: "Please Link your project repository",
+    default: "Repository Link",
+    name: "tertius",
+  },
+  {
+    type: "input",
+    message: "Please Link your deployed application",
+    default: "Deployed App Link",
+    name: "quartus",
+  },
+  {
+    type: "checkbox",
+    message: "Which technologies were used?",
+    name: "quintus",
+    choices: ["HTML", "CSS", "JavaScript", "jQuery", "Node", "Python"],
+  },
+  {
+    type: "input",
+    message: "Please Enter a User Story",
+    default: "Here be User Stories",
+    name: "sextus",
+  }
+];
 
-    // TODO: Create a function to write README file
-    .then((response) => {
-        console.log(response)
-        const readmestring = generateREADME(response)
-        console.log(readmestring)
-        fs.writeToFile("README.md", readmestring, err => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log('IT WORK!')
-            }
-        })
-    })
-
-
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utilities/generateMarkdown.js
+++ Develop/utilities/generateMarkdown.js
@@ -1,20 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
-
-`;
+  // semi-colens are not your friend
+  return `#${data.primus} 
+    
+  
+      ## User Story
+      ${data.sextus}
+  
+  
+      ## Description
+      ${data.secundus}
+  
+      ## Links to Repo and Deployed Site
+      Link to repo: ${data.tertius}
+      Link to deployed app: ${data.quartus}
+  
+      ### Technologies Used
+      ${data.quintus}`;
 }
 
 module.exports = generateMarkdown;
